6b81e1d3fcd9a8857f83c4a69a27ad222c76f022
Tyler Mitchell <gravdanger510@gmail.com>
2018-05-16 19:49:25 -0700

    forms work

--- source/javascripts/site.js
+++ source/javascripts/site.js
@@ -1,1 +1,28 @@
-// This is where it all goes :)
+// Initialize Firebase
+var config = {
+  apiKey: "AIzaSyC7cYs3KKSuIUCHnKEl80kJee6VicnkWow",
+  authDomain: "yay-tweets.firebaseapp.com",
+  databaseURL: "https://yay-tweets.firebaseio.com",
+  projectId: "yay-tweets",
+  storageBucket: "yay-tweets.appspot.com",
+  messagingSenderId: "240663594792"
+};
+firebase.initializeApp(config);
+
+var database = firebase.database(),
+nums = database.ref('nums'),
+remove = database.ref('removeMe');
+
+function addNum(n) {
+  nums.push({
+    digits: n
+  });
+  console.log("added " + n);
+}
+
+function removeNum(n) {
+  remove.push({
+    digits: n
+  });
+  console.log("added " + n);
+}
